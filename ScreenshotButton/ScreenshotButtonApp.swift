0cbf65ac799a41dc07a2b55e78547dbe3c3dfc03
Box o' Rocks <b0x0fr0cks@pm.me>
2022-03-07 16:26:26 -0500

    Initial Commit

---
+++ ScreenshotButton/ScreenshotButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotButtonApp.swift
+//  ScreenshotButton
+//
+//  Created by laptop on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
