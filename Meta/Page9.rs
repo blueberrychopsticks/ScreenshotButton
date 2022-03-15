8d887ff93af42e87b6e3c937fbfcff0ff3a6a7e6
Box o' Rocks <b0x0fr0cks@pm.me>
2022-03-15 12:31:38 -0400

    Create Page 9; Stretch.md

---
+++ Meta/Page9.rs
@@ -0,0 +1,33 @@
+////INPUT GOAL: 
+/// create a parser in file's language 
+/// that parser should take the output of the file labled from `////INPUT GOAL: (GoalDescription)` until `////OUTPUT` 
+import global::chain::{Times, World, Record, TargetAquisition}
+// import global::{Times, World, Record, TargetAquisition}
+///`start March 15, 2022 1:20 AM`
+
+///`current March 15, 2022 2:06 AM`
+
+// .                            <- this equal would be ignored everything after and that everything after could be used to reference this line
+
+///// PARSER GOAL - add macro that handles second equals, performing desired action parsing while
+///  also adding the semicolon to the end and returning a public github reference to that 
+///  very line
+/// -
+///  also add
+/////  
+///// the line and also parses your command
+// ex: 
+/////DOCUMENTATION MUST BE THIS GOOD AND HAVE EXAMPLES.!MANDATORY 
+let duration = current - start = 46 minutes. world record(reason: “nobody else was playing”)`
+
+let target = b`let’s play!`;
+
+`loop {
+ while !target.acqured target.check()
+}
+
+    todo!('unreachable until game playable on chain').fetch(git commit => [blockchain type => blockchain address]
+[
+
+
+////OUTPUT
---
+++ Meta/STRETCH.md
@@ -0,0 +1,9 @@
+# Stretch Goals and Brainstorm Ideas
+
+What if there could be a multi linked way of handling TODOS?
+todos file in the repo
+todos entry in a code file
+todos entry in JIRA
+todos entry in GitHub pages
+todos entry in Notion, YATA (yet another todo app)
+Everything syncs when a todo is added, completed, or otherwise modified
